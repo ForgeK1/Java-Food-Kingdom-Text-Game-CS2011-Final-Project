4cdc7850898e56b10cf711606320454530747a36
Keyvan Mahmoodzadeh Kani <arasanjkk@gmail.com>
2022-12-08 14:44:56 -0800

    I updated my homeless mass minigame

--- Minigames/HomelessMiniGame.java
+++ Minigames/HomelessMiniGame.java
@@ -6,26 +6,23 @@ public class HomelessMiniGame
   {
     Scanner input = new Scanner(System.in);
 
+    //Story of the minigame
     System.out.println("Suddenly a homeless man jumps in front of you! He says "
-                       + "\"Let's do rock paper scirrors right now or else "
+                       + "\"Let's do rock paper scissors right now or else "
                        + "I will take all of your groceries! If I win then "
                        + "I will take one item from your cart, if you win "
                        + "then you'll be left alone.\"" + " You have no "
                        + "other choice but to play so choose your action "
                        + "carefully.");
 
+    //Food item the user will recieve
     String food = "Orange";
-    // boolean[] items = new boolean[3];
-    //
-    // final int Cucumber = 1;
-    // final int Apple = 1;
-    // final int Bannana = 1;
-    //
-    // items = Cucumber, Apple, Bannana;
 
+    //A do while loop that ends if the user or the homeless man wins
     boolean homelessManTiesWithUser = true;
     do
     {
+      //Prompts the user to enter an input
       System.out.print("\nscissor (0), rock (1), paper (2): ");
       int userInput = input.nextInt();
       String userInputString = "";
